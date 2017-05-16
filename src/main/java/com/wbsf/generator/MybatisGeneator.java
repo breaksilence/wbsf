24f02fad82e9f3aa11e9bef52a54aabe0a93c4d2
xiangzheng <xiangzheng@comisys.net>
2017-05-16 19:35:34 +0800

    提交代码生成器

--- src/main/java/com/wbsf/generator/MybatisGeneator.java
+++ src/main/java/com/wbsf/generator/MybatisGeneator.java
@@ -9,14 +9,18 @@ import org.mybatis.generator.config.xml.ConfigurationParser;
 import org.mybatis.generator.internal.DefaultShellCallback;
 
 public class MybatisGeneator {
-	 public static void main(String[] args) throws Exception {
-	        List<String> warnings = new ArrayList<String>();
-	        boolean overwrite = true;
-	        ConfigurationParser cp = new ConfigurationParser(warnings);
-	        Configuration config = cp.parseConfiguration(
-	        		MybatisGeneator.class.getResourceAsStream("/generator/generatorConfig.xml"));
-	        DefaultShellCallback callback = new DefaultShellCallback(overwrite);
-	        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
-	        myBatisGenerator.generate(null);
-	    }
+	
+	public static void generator() throws Exception {
+		List<String> warnings = new ArrayList<String>();
+		boolean overwrite = true;
+		ConfigurationParser cp = new ConfigurationParser(warnings);
+		Configuration config = cp
+				.parseConfiguration(MybatisGeneator.class.getResourceAsStream("/generator/config/generatorConfig.xml"));
+		DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+		MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+		myBatisGenerator.generate(null);
+	}
+	public static void main(String[] args) throws Exception {
+		generator();
+	}
 }
