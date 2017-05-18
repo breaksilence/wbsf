c351e18819205ded12f3a064e6368ea164c1b764
hubery <hubery@LAPTOP-440QRPVN>
2017-05-19 00:23:45 +0800

    新增日期格式化单元测试用例

--- src/test/java/com/wbsf/modules/test/controller/TestControllerTest.java
+++ src/test/java/com/wbsf/modules/test/controller/TestControllerTest.java
@@ -65,6 +65,30 @@ public class TestControllerTest extends JunitTestSupport{
 		.andExpect(jsonPath("$.isSuccess", is(true)));
 		logger.info("测试demo insert---end>>>time:"+(System.currentTimeMillis()-startTime));
 	}
+	
+	/**
+	 * 验证正常插入数据
+	 * @throws Exception
+	 */
+	@Test
+	public void testDateConvert() throws Exception {
+		logger.info("测试demo insert");
+		Long startTime = System.currentTimeMillis();
+		logger.info("测试demo insert---start>>>time:"+startTime);
+		this.mockMvc
+		.perform(post("/test/insert").characterEncoding("UTF-8") 
+				.accept(MediaType.APPLICATION_JSON_UTF8_VALUE)
+				.param("name", "testName01")
+				.param("code", "testCode01")
+				.param("modifyTime", "2017-05-18 00:19:21")
+				)
+		.andDo(print())
+		.andExpect(status().isOk())
+		.andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+		.andExpect(jsonPath("$.resultCode", is("success")))
+		.andExpect(jsonPath("$.isSuccess", is(true)));
+		logger.info("测试demo insert---end>>>time:"+(System.currentTimeMillis()-startTime));
+	}
 
 	/**
 	 * 测试英文国际化
