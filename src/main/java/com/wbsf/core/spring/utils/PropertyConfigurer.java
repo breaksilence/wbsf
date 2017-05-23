27c6ba20e6e0360f21092e2222c24832fdf1d945
hubery <hubery@LAPTOP-440QRPVN>
2017-05-23 23:14:42 +0800

    增加配置文件读取工具类注释

--- src/main/java/com/wbsf/core/spring/utils/PropertyConfigurer.java
+++ src/main/java/com/wbsf/core/spring/utils/PropertyConfigurer.java
@@ -10,37 +10,42 @@ import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
 
 /**
  * spring properties配置文件读取类
+ * 
  * @author xiangzheng
  *
  */
 public class PropertyConfigurer extends PropertyPlaceholderConfigurer {
-	private static Properties props;       // 存取properties配置文件key-value结果
+	private static Properties props; // 存取properties配置文件key-value结果
 	private static final Logger logger = LogManager.getLogger(PropertyConfigurer.class.getName());
-    @Override
-    protected void processProperties(ConfigurableListableBeanFactory beanFactoryToProcess, Properties props)
-                            throws BeansException {
-        super.processProperties(beanFactoryToProcess, props);
-        if(PropertyConfigurer.props == null)
-        	PropertyConfigurer.props = props;
-        logger.info("配置文件加载状态为："+getProperty("property.init.check","init properties failed"));
-    }
-    
-    /**
-     * 通过key获取value
-     * @param key properties key
-     * @return
-     */
-    public static String getProperty(String key){
-        return props.getProperty(key);
-    }
 
-    /**
-     * 根据key 获取value,如果没有获取到设置为默认值
-     * @param key
-     * @param defaultValue
-     * @return String 
-     */
-    public static String getProperty(String key, String defaultValue) {
-        return props.getProperty(key, defaultValue);
-    }
+	@Override
+	protected void processProperties(ConfigurableListableBeanFactory beanFactoryToProcess, Properties props)
+			throws BeansException {
+		super.processProperties(beanFactoryToProcess, props);
+		if (PropertyConfigurer.props == null)
+			PropertyConfigurer.props = props;
+		logger.info("配置文件加载状态为：" + getProperty("property.init.check", "init properties failed"));
+	}
+
+	/**
+	 * 通过key获取value
+	 * 
+	 * @param key
+	 *            properties key
+	 * @return 返回配置文件key所对应的值
+	 */
+	public static String getProperty(String key) {
+		return props.getProperty(key);
+	}
+
+	/**
+	 * 根据key 获取value,如果没有获取到设置为默认值
+	 * 
+	 * @param key
+	 * @param defaultValue
+	 * @return 返回配置文件key所对应的值，如果没有返回defualt
+	 */
+	public static String getProperty(String key, String defaultValue) {
+		return props.getProperty(key, defaultValue);
+	}
 }
