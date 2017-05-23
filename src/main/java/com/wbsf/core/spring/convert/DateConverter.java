034e548a1fc21ba2dc515635882c731ad3bf3742
hubery <hubery@LAPTOP-440QRPVN>
2017-05-23 23:38:41 +0800

    修改日期类名称，排除findbugs扫描警告

--- src/main/java/com/wbsf/core/spring/convert/DateConverter.java
+++ src/main/java/com/wbsf/core/spring/convert/DateConverter.java
@@ -7,7 +7,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.core.convert.converter.Converter;
 
-import com.wbsf.common.utils.DateUtils;
+import com.wbsf.common.utils.DateUtil;
 
 /**
  * Spring 日期转换器
@@ -18,7 +18,7 @@ public class DateConverter implements Converter<String, Date> {
 	private Logger logger = LogManager.getLogger(getClass());
 	@Override  
 	public Date convert(String source) {
-		Date bindDate = DateUtils.parseDate(source);
+		Date bindDate = DateUtil.parseDate(source);
 		if(StringUtils.isNotBlank(source) && bindDate == null)
 			logger.error("日期类型数据绑定异常，尝试将"+source+"转换为Date类型，但是没有成功!");
 		return bindDate;
