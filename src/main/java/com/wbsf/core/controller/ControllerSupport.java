890f4476b9a1cb7d9f885a4f8aa4b4d5c3453711
xiangzheng <suiyisi@163.com>
2017-06-06 18:59:47 +0800

    移除多余的包引用

--- src/main/java/com/wbsf/core/controller/ControllerSupport.java
+++ src/main/java/com/wbsf/core/controller/ControllerSupport.java
@@ -11,7 +11,6 @@ import javax.servlet.http.HttpServletResponse;
 import javax.validation.ConstraintViolationException;
 import javax.validation.ValidationException;
 
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.BindException;
 import org.springframework.validation.BindingResult;
