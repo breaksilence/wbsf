bb37ee6a40e980f1a09171b90b02cc0be335d537
xiangzheng <suiyisi@163.com>
2017-06-07 11:56:20 +0800

    修改基础实体类的包路径

---
+++ src/main/java/com/wbsf/core/persistence/BaseEntity.java
@@ -0,0 +1,17 @@
+package com.wbsf.core.persistence;
+
+import java.io.Serializable;
+
+/**
+ * 基础实体类
+ * @author hubery
+ *
+ */
+public class BaseEntity implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3791617830666435304L;
+
+}
---
+++ src/main/java/com/wbsf/core/persistence/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * 持久化基础对象定义
+ * @author xiangzheng
+ *
+ */
+package com.wbsf.core.persistence;
