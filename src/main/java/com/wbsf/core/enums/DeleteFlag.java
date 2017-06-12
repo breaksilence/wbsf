738e3d69315506a3275a8eb4bbdd4ad4be1d2d9d
hubery <suiyisi@163.com>
2017-06-12 23:52:16 +0800

    增加删除枚举注释

--- src/main/java/com/wbsf/core/enums/DeleteFlag.java
+++ src/main/java/com/wbsf/core/enums/DeleteFlag.java
@@ -6,14 +6,19 @@ package com.wbsf.core.enums;
  *
  */
 public enum DeleteFlag {
-	NORMAL(1),DELETE(0),DELETE_FORBIDDEN(-1);
+	/** 文件正常 */
+	NORMAL(1),
+	/** 删除 */
+	DELETE(0),
+	/** 系统标识，禁止删除 */
+	DELETE_FORBIDDEN(-1);
 	private int deleteFlag ;
 	private DeleteFlag(int deleteFlag){
 		this.deleteFlag = deleteFlag;
 	}
 	
 	/**
-	 * 获取枚举对应的值
+	 * 获取删除对应的值
 	 */
 	public int flag(){
 		return deleteFlag;
